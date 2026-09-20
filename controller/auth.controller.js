aab31a35f0745ada2a9b8df182a5d030e6df97c4
KarlusFY <156101853+KarlusFY@users.noreply.github.com>
2026-09-20 09:44:20 +0000

    Correction pour le renvoi d'email

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -14,6 +14,7 @@ import {
 } from '../services/password-reset.service.js';
 import {
   consumeEmailVerificationToken,
+  createEmailVerificationToken,
 } from '../services/email-verification.service.js';
 import { sendEmail } from '../services/mail.service.js';
 
